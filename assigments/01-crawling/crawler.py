69cc1ad8dbbab56cc763377edd005a75c9d4a6bd
Vratislav Bartonicek <vratislav@vbartonicek.cz>
2018-03-10 19:43:51 +0100

    Fixes: phases + players without statistics page

--- assigments/01-crawling/crawler.py
+++ assigments/01-crawling/crawler.py
@@ -12,78 +12,78 @@ class ChampionsLeagueSpider(scrapy.Spider):
         'DOWNLOAD_DELAY': 0.001,
     }
 
+    # Parse statistics data of the current season
+    def parse_phase(self, fields):
+        phase = {}
+        for field in fields:
+            label = field.css('.statistics--list--label::text').extract_first()
+
+            # Passing accuracy field
+            if (label == 'passing accuracy'):
+                value = field.css('.graph-circle--additional-text::text').extract_first()
+
+            # Cards field
+            elif (label == 'Cards'):
+                cards = field.xpath('.//span[@class="statistics--list--data"]/text()').extract()
+
+                yellow = re.findall('\d+', cards[0])
+                red = re.findall('\d+', cards[1])
+
+                value = {
+                    'yellow': yellow[0],
+                    'red': red[0],
+                }
+
+            # Passing types field
+            elif (label == 'Passing types'):
+                types = field.css('.statistics--list--data .graph-bar-container .bar-container')
+                value = {}
+
+                for type in types:
+                    pass_type = type.css('span:not(.bar)::text').extract_first()
+                    type_label = pass_type.split(' ')[1].lower()
+                    type_value = type.css('span:not(.bar) b::text').extract_first() + ' ' + \
+                                 pass_type.split(' ')[2]
+                    value[type_label] = type_value
+
+            # Goal types field
+            elif (label == 'Type of goal'):
+                types = field.css('.statistics--list--data .graph-dummy-container > div')
+                value = {}
+                for type in types:
+                    type_label = type.xpath('.//span/text()').extract()
+                    type_value = type.css('div > span::text').extract_first()
+                    value[type_label[1]] = type_value
+
+            # Standard fields
+            else:
+                value = field.css('.statistics--list--data::text').extract_first()
+
+            phase[label.lower().replace(" ", "-")] = value
+
+        return phase
+
+    # Collect statistics data and split it into phases
     def parse_stats(self, response):
         for stats in response.xpath('//div[@class="content-wrap"]'):
-
             player = response.meta['player']
             stats_sections = stats.css('.player--statistics--list')
-
-            tournament = {}
-            qualification = {}
+            player['matches'] = {}
 
             for section in stats_sections:
                 fields = section.css('.field')
 
                 # Tournament phase
                 if (stats.css('.stats-header::text').extract_first() == 'Tournament phase'):
-                    for field in fields:
-                        label = field.css('.statistics--list--label::text').extract_first()
-
-                        # Passing accuracy
-                        if (label == 'passing accuracy'):
-                            value = field.css('.graph-circle--additional-text::text').extract_first()
-
-                        # Cards
-                        elif (label == 'Cards'):
-                            cards = field.xpath('.//span[@class="statistics--list--data"]/text()').extract()
-
-                            yellow = re.findall('\d+', cards[0])
-                            red = re.findall('\d+', cards[1])
-
-                            value = {
-                                'yellow': yellow[0],
-                                'red': red[0],
-                            }
-
-                        # Passing types
-                        elif (label == 'Passing types'):
-                            types = field.css('.statistics--list--data .graph-bar-container .bar-container')
-                            value = {}
-
-                            for type in types:
-                                pass_type = type.css('span:not(.bar)::text').extract_first()
-                                type_label = pass_type.split(' ')[1].lower()
-                                type_value = type.css('span:not(.bar) b::text').extract_first() + ' ' + \
-                                             pass_type.split(' ')[2]
-                                value[type_label] = type_value
-
-                        # Goal types
-                        elif (label == 'Type of goal'):
-                            types = field.css('.statistics--list--data .graph-dummy-container > div')
-                            value = {}
-                            for type in types:
-                                type_label = type.xpath('.//span/text()').extract()
-                                type_value = type.css('div > span::text').extract_first()
-                                value[type_label[1]] = type_value
-
-                        else:
-                            value = field.css('.statistics--list--data::text').extract_first()
-
-                        tournament[label.lower().replace(" ", "-")] = value
+                    player['matches']['tournament'] = self.parse_phase(fields)
 
                 # Qualification phase
                 elif (stats.css('.stats-header::text').extract_first() == 'Qualifying'):
-                    for field in fields:
-                        qualification[field.css('.statistics--list--label::text').extract_first()] = field.css(
-                            '.statistics--list--data::text').extract_first()
-
-        player['matches'] = {
-            'tournament': tournament,
-            'qualification:': qualification,
-        }
+                    player['matches']['qualification'] = self.parse_phase(fields)
 
         yield player
 
+    # Visit player's profile page, collect biography data and get a link to statistics page
     def parse_player(self, response):
         stats_url = response.css(
             '.content-wrap .section--footer a[title="More statistics"]::attr(href)').extract_first()
@@ -99,17 +99,22 @@ class ChampionsLeagueSpider(scrapy.Spider):
                 'weight': player.css('.profile--list--data[itemprop=weight]::text').extract_first(),
             }
 
-            yield scrapy.Request(response.urljoin(stats_url), callback=self.parse_stats, meta={'player': bio})
+            if (stats_url):
+                yield scrapy.Request(response.urljoin(stats_url), callback=self.parse_stats, meta={'player': bio})
+            else:
+                yield bio
 
+    # Visit page of each club and collect links to players profiles
     def parse_clubs(self, response):
-        players = response.css('#team-data .squad--team-player')
+        players = response.css('#team-data:first-child .squad--team-player')
         players_urls = players.css('.squad--player-name > a::attr(href)').extract()
 
         for player_url in players_urls:
             yield scrapy.Request(response.urljoin(player_url), callback=self.parse_player)
 
+    # Get a list of clubs from the starting page
     def parse(self, response):
-        clubs = response.css('.team > a')
+        clubs = response.css('.teams-overview_group .team > a')
 
         clubs_tocrawl = clubs.css('::attr(href)').extract()
 
